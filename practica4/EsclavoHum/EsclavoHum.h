654da6d70c14a2ee134901aef798487192453cdc
carrodher <carrodher1179@gmail.com>
2016-03-26 14:56:26 +0100

    Modificados todos los .h

--- practica4/EsclavoHum/EsclavoHum.h
+++ practica4/EsclavoHum/EsclavoHum.h
@@ -2,22 +2,25 @@
 #define ESCLAVO_H
 
 enum {
-	AM_ESCLAVO 		= 13,
-	MAESTRO_ID 		= 131,
-	ESCLAVO_ID 		= 132,
-	TEMPERATURA		= 1,
-	HUMEDAD			= 2,
-	LUMINOSIDAD		= 3
+	AM_ESCLAVO 			= 13,
+	MAESTRO_ID 			= 130,
+	ESCLAVO_TEMP_ID    	= 131,
+	ESCLAVO_HUM_ID    	= 132,
+	ESCLAVO_LUM_ID    	= 133,
+	TEMPERATURA			= 1,
+	HUMEDAD				= 2,
+	LUMINOSIDAD			= 3
 };
 
 typedef nx_struct MaestroMsg {
 	nx_uint16_t ID_maestro;		// ID maestro = ID origen
-	nx_uint16_t ID_esclavo;		// ID esclavo = ID destino
-	nx_uint16_t tipo;			// Tipo de medida que solicita
+	nx_uint16_t first;			// 1º slot para este esclavo
+	nx_uint16_t second;			// 2º slot para este esclavo
+	nx_uint16_t third;			// 3º slot para este esclavo
 } MaestroMsg;
 
 typedef nx_struct EsclavoMsg {
-	nx_uint16_t ID_esclavo;		// ID esclavo = ID origne
+	nx_uint16_t ID_esclavo;		// ID esclavo = ID origen
 	nx_uint16_t medidaRssi;		// Valor de la medida RSSI
 	nx_uint16_t tipo;			// Tipo de medida solicitada
 	nx_uint16_t medida;			// Valor de la medida solicitada
--- practica4/Jefe/Jefe.h
+++ practica4/Jefe/Jefe.h
@@ -4,8 +4,10 @@
 enum {
 	TIMER_PERIOD_MILLI 	= 4000,
 	AM_MAESTRO  	   	= 13,
-	MAESTRO_ID  	   	= 131,
-	ESCLAVO_ID 		   	= 132,
+	MAESTRO_ID  	   	= 130,
+	ESCLAVO_TEMP_ID    	= 131,
+	ESCLAVO_HUM_ID    	= 132,
+	ESCLAVO_LUM_ID    	= 133,
 	TEMPERATURA			= 1,
 	HUMEDAD				= 2,
 	LUMINOSIDAD			= 3
@@ -13,12 +15,13 @@ enum {
 
 typedef nx_struct MaestroMsg {
 	nx_uint16_t ID_maestro;		// ID maestro = ID origen
-	nx_uint16_t ID_esclavo;		// ID esclavo = ID destino
-	nx_uint16_t tipo;			// Tipo de medida que solicita
+	nx_uint16_t first;			// 1º slot para este esclavo
+	nx_uint16_t second;			// 2º slot para este esclavo
+	nx_uint16_t third;			// 3º slot para este esclavo
 } MaestroMsg;
 
 typedef nx_struct EsclavoMsg {
-	nx_uint16_t ID_esclavo;		// ID esclavo = ID origne
+	nx_uint16_t ID_esclavo;		// ID esclavo = ID origen
 	nx_uint16_t medidaRssi;		// Valor de la medida RSSI
 	nx_uint16_t tipo;			// Tipo de medida solicitada
 	nx_uint16_t medida;			// Valor de la medida solicitada
